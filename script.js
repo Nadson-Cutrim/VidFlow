3844e7bc1d1734a53b2031a62d2b25001c6e9220
Nadson Rodrigues Cutrim Junior <nadsoncutrimjunior@aluno.uema.br>
2025-04-20 01:04:41 -0300

    Adicionando videos de Anime em outro servidor json

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const containerVideos = document.querySelector(".videos__container"); // Seleciona o container onde os vídeos serão inseridos
+const api3000 = fetch("http://localhost:3000/animes")
+.then(res => res.json()) // Converte a resposta em JSON
+.then((animes) => 
+    animes.forEach(animes => {
+containerVideos.innerHTML += ` 
+<li class="videos__item">
+<iframe src="${animes.url}" title="${animes.titulo}" frameborder="0" allowfullscreen></iframe> 
+<div class="descricao-video">
+<img class="img-canal" src="${animes.imagem}" alt="Logo do canal ${animes.canal}">
+<h3 class="titulo-video">${animes.titulo}</h3>
+<p class="descricao-video">${animes.descricao}</p>
+</div>
+</li>
+`
+    })
+)
 
-const api = fetch("http://localhost:3000/videos") // Faz uma requisição para a API local que retorna os vídeos
+const api = fetch( "http://localhost:3001/videos") // Faz uma requisição para a API local que retorna os vídeos
 .then(res => res.json()) // Converte a resposta em JSON
 .then((videos) => 
     videos.forEach(video => {
@@ -17,5 +33,6 @@ containerVideos.innerHTML += `
     }) // Adiciona o HTML de cada vídeo no container
 )
 .catch((error) =>{
-    containerVideos.innerHTML = `<p>Erro ao carregar os vídeos: ${error}</p>` // Caso ocorra um erro, exibe uma mensagem de erro
-})
+    containerVideos.innerHTML = `<p style="color:red">Erro ao carregar os vídeos: ${error}. Por favor, tente novamente mais tarde.</p>` // Caso ocorra um erro, exibe uma mensagem de erro
+}) 
+
