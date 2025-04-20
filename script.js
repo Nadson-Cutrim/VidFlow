080db5eb1f7fd2d7c7f92a81f5fd3d1bfbf3fb16
Nadson Rodrigues Cutrim Junior <nadsoncutrimjunior@aluno.uema.br>
2025-04-19 22:22:33 -0300

    Exibindo os vídeos na aplicação

---
+++ script.js
@@ -0,0 +1,18 @@
+const containerVideos = document.querySelector(".videos__container"); // Seleciona o container onde os vídeos serão inseridos
+
+const api = fetch("http://localhost:3000/videos") // Faz uma requisição para a API local que retorna os vídeos
+.then(res => res.json()) // Converte a resposta em JSON
+.then((videos) => 
+    videos.forEach(video => {
+containerVideos.innerHTML += ` 
+<li class="videos__item">
+<iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe> 
+<div class="descricao-video">
+<img class="img-canal" src="${video.imagem}" alt="Logo do canal ${video.canal}">
+<h3 class="titulo-video">${video.titulo}</h3>
+<p class="descricao-video">${video.descricao}</p>
+</div>
+</li>
+`
+    }) // Adiciona o HTML de cada vídeo no container
+)
