ec476db1e1f6a1dc0d5500c17b89658f38a816c8
Nadson Rodrigues Cutrim Junior <nadsoncutrimjunior@aluno.uema.br>
2025-04-22 23:09:32 -0300

    Iplementando lógica da barra de pesquisa

--- script.js
+++ script.js
@@ -33,8 +33,10 @@ async function buscareMostrarVideos() {
                 <div class="descricao-video">
                 <img class="img-canal" src="${video.imagem}" alt="Logo do canal ${video.canal}">
                 <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="categoria" hidden>${video.categoria}</p>
                 <p class="descricao-video">${video.descricao}</p>
                 </div>
+                
                 </li>`;
     }); // Adiciona o HTML de cada vídeo no container
   } catch (error) {
@@ -42,5 +44,29 @@ async function buscareMostrarVideos() {
   }
 }
 buscareMostrarVideos();
+const barraDePesquisa = document.querySelector(".pesquisar__input"); // Seleciona a barra de pesquisa
+barraDePesquisa.addEventListener("input",filtrarPesquisa); // Adiciona um evento de input à barra de pesquisa
+function filtrarPesquisa(){
+    const videos = document.querySelectorAll(".videos__item");
+    if (barraDePesquisa.value != "") {
+       for(let video of videos){
+        let tituloVideo = video.querySelector(".titulo-video").textContent.toLowerCase();
+        let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+        if(!tituloVideo.includes(valorFiltro)){
+            video.style.display = "none"; // Se o título do vídeo não incluir o valor da barra de pesquisa, oculta o vídeo
+        }else{
+            video.style.display = "block"; // Caso contrário, exibe o vídeo
+        }
+       }
+    } else{
+        video.style.display = "block"; // Se a barra de pesquisa estiver vazia, exibe todos os vídeos
+    }
+}
+
+
+
+
+
 // json-server --watch backend/animes.json --port 3000 para rodar o json-server na porta 3000
 // json-server --watch backend/videos.json --port 3001 para rodar o json-server na porta 3001
